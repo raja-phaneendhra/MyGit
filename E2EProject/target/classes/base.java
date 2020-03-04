40976cdcf2d61311f4fca79596d9d8e0ad6be82b
raja-phaneendhra <raja.phaneendhra@gmail.com>
2020-03-04 22:39:24 +0530

    Second Commit

--- E2EProject/target/classes/base.java
+++ E2EProject/target/classes/base.java
@@ -22,7 +22,7 @@ public WebDriver initializeDriver() throws IOException
 {
 	
  prop= new Properties();
-FileInputStream fis=new FileInputStream("/Users/Raja/E2EProject/src/main/java/resources/data.properties");
+FileInputStream fis=new FileInputStream("/Users/Raja/git/MyGit/E2EProject/src/main/java/resources/data.properties");
 
 prop.load(fis);
 String browserName=prop.getProperty("browser");
